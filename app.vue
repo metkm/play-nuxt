bdf622d87ab71b98c84fe9eb07340424fbc1f3ed
metkm <metinkorkmaz417@gmail.com>
2024-01-25 23:37:59 +0300

    init

---
+++ app.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <Suspense>
+      <BaseIcon name="Settings" :size="24" />
+    </Suspense>
+  </div>
+</template>
---
+++ components/base/Icon.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts" generic="T extends keyof GlobalComponents">
+import type { GlobalComponents } from 'vue';
+
+const props = defineProps<{
+  name: T extends `Icon${infer N}` ? N : never,
+  size?: 16 | 24
+}>();
+
+const Icon = defineAsyncComponent(() => import(`~/components/icon/${props.name}.vue`));
+</script>
+
+<template>
+  <component :is="Icon" />
+</template>
